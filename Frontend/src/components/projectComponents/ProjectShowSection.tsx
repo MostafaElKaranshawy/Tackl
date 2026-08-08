49255e766632a9d49c672936e454205660cae923
Mostafa Elkaranshawy <mostafaelkaranshawy0@gmail.com>
2026-08-08 20:49:47 +0300

    TT26-118 ES-Lint Frontend Files Edits Edit frontend files to follow eslint standards

--- Frontend/src/components/projectComponents/ProjectShowSection.tsx
+++ Frontend/src/components/projectComponents/ProjectShowSection.tsx
@@ -8,6 +8,7 @@ import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 export default function ProjectShowSection() {
+
     const navigate = useNavigate();
     const { projectId } = useParams<{ projectId: string }>();
     const [project, setProject] = useState<Project | null>(null);
