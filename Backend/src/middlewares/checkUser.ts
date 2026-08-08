44050e73772bd28f4c6c91134ffcd6abfe48772e
Mostafa Elkaranshawy <mostafaelkaranshawy0@gmail.com>
2026-08-08 20:51:15 +0300

    TT26-118 Fix Authentication Bug Remove the invalid cookie in the `checkUser.ts` middleware to make the user re-login again to refresh his token.

--- Backend/src/middlewares/checkUser.ts
+++ Backend/src/middlewares/checkUser.ts
@@ -11,6 +11,11 @@ export default async function checkUser(req: Request, res: Response, next: () =>
     try {
         const tokenData = Jwt.verifyToken(token);
         if (!tokenData) {
+            res.clearCookie("accessToken", {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: "strict",
+            });
             return res.status(401).json({ message: "Unauthorized" });
         }
 
@@ -18,11 +23,21 @@ export default async function checkUser(req: Request, res: Response, next: () =>
         req.tokenPurpose = tokenData.purpose;
 
         if (req.tokenPurpose !== "accessToken") {
+            res.clearCookie("accessToken", {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: "strict",
+            });
             return res.status(403).json({ message: "Forbidden" });
         }
         next();
     } catch (error) {
         logger.error("Error in checkUser middleware:", error);
+        res.clearCookie("accessToken", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+        });
         return res.status(401).json({ message: "Invalid token" });
     }
 }
