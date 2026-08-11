f544440758e27ef760fb5555d0d81704d06a889d
Mostafa Elkaranshawy <mostafaelkaranshawy0@gmail.com>
2026-08-11 23:38:10 +0300

    TT26-143 Backend Middlewares, Repositories Unit Testing Done with 99% Coverage

--- Backend/vitest.config.ts
+++ Backend/vitest.config.ts
@@ -11,6 +11,9 @@ export default defineConfig({
                 "html",
                 "lcov",
             ],
+            include: [
+                "src/**",
+            ],
             exclude: [
                 "tests/**",
                 "src/config/**",
